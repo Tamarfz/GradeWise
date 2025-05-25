317b6e0a31bf667f4e649baafd10b0d872e1924c
idoBeitOn <130286681+idoBeitOn@users.noreply.github.com>
2025-05-25 10:48:20 +0300

    Delete mta-final-projects-site directory

--- mta-final-projects-site/src/config.js
+++
@@ -1,3 +0,0 @@
-const backendURL = process.env.REACT_APP_BACKEND_URL;
-
-export { backendURL };
--- mta-final-projects-site/src/stores/index.js
+++
@@ -1,7 +0,0 @@
-import userStorage from './UserStorage';
-import appStorage from './AppStorage';
-
-export const storages = {
-  userStorage,
-  appStorage
-};
